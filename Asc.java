476ee3a63b43d3dfb0c159f69f144d7b4d7224a7
vikasvks90 <vikas.varshney90@gmail.com>
2017-04-28 18:43:33 +0530

    commit

---
+++ Asc.java
@@ -0,0 +1,15 @@
+package Assignment1;
+
+public class Asc {
+
+	public static void main(String[] args) {
+		System.out.println("List of ASCII Characters are given below: ");
+        
+        for(int i = 65; i <= 90; i++)
+        {
+             System.out.println(i+"::"+(char)i);
+        }
+
+	}
+
+}
